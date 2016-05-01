315881ea5dd42e9246fc99600972b905fd35873f
0xFN89 <eighteen089@gmail.com>
2016-05-01 17:11:16 +0530

    added padding around form

--- chattr.py
+++ chattr.py
@@ -3,7 +3,7 @@ from flask.ext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']='rohit123'
+app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='chattr'
 app.config['MYSQL_DATABASE_HOST']='localhost'
 mysql.init_app(app)
