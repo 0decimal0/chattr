2a0b47447b570544960150383f4d454af68fc387
0xFN89 <eighteen089@gmail.com>
2016-05-01 17:16:52 +0530

    removed unnecessary code for db access

--- chattr.py
+++ chattr.py
@@ -2,17 +2,9 @@ from flask import Flask ,render_template
 from flask.ext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_DB']='chattr'
-app.config['MYSQL_DATABASE_HOST']='localhost'
-mysql.init_app(app)
 
 @app.route("/")
 def hello():
     return "database connected"
-@app.route("/login")
-def signin():
-    return render_template('login.html')
 if __name__=='__main__':
     app.run()
