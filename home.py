dd61ab9b41bcd9087b27f30cdc99936bdecb8330
0xFN89 <eighteen089@gmail.com>
2016-05-03 20:51:38 +0530

    used templates

--- home.py
+++ home.py
@@ -3,13 +3,14 @@ from connector import connect
 
 app = Flask(__name__)
 
-@app.route("/test")
+@app.route("/home")
 def hello():
     db=connect()
     cursor = db.cursor()
-    cursor.execute("select username from user")
-    uname = cursor.fetchone()
-    return uname
+    cursor.execute("select * from user")
+    user = [dict(uname=row[1],upass=row[2]) for row in cursor.fetchall()]
+    return render_template('home.html',user=user)
 if __name__=='__main__':
     app.run()
+
 
--- login.py
+++ login.py
@@ -1,7 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session,g,url_for,redirect,flash,abort
+from connector import connect
+
 app=Flask(__name__)
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def login():
+    error = None
+    uname = request.form['username']
+    upass = request.form['password']
+    db = connect()
+    cursor = db.cursor()
+    result = cursor.execute("select username,password from user where email=%s and password=%s",uname,upass)
+    if result == True:
+        return redirect(url_for('home.html'))
     return render_template("login.html")
 if __name__=='__main__':
     app.run()
