c41ca78662f10ad5b34f72d342e97171ae3ec134
0xFN89 <eighteen089@gmail.com>
2016-05-05 20:34:30 +0530

    minor changes

--- login.py
+++ login.py
@@ -2,16 +2,19 @@ from flask import Flask,render_template,request,session,g,url_for,redirect,flash
 from connector import connect
 
 app=Flask(__name__)
-@app.route("/",methods=['GET','POST'])
+@app.route("/login",methods=['GET','POST'])
 def login():
+    return render_template('login.html')
+@app.route("/home",methods=['GET','POST'])
+def home():
     error = None
     uname = request.form['username']
     upass = request.form['password']
-    db = connect()
+    '''db = connect()
     cursor = db.cursor()
     result = cursor.execute("select username,password from user where email=%s and password=%s",uname,upass)
     if result == True:
-        return redirect(url_for('home.html'))
-    return render_template("login.html")
+        return redirect(url_for('home.html'))'''
+    return render_template("home.html",user=uname,passw=upass)
 if __name__=='__main__':
     app.run()
