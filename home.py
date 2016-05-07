2cbdc026f32c8769b36552e063ff0b980642f2f1
0xFN89 <eighteen089@gmail.com>
2016-05-07 14:33:18 +0530

    login form validation check

--- home.py
+++ home.py
@@ -1,15 +1,20 @@
 from flask import Flask ,render_template
 from connector import connect
+import hashlib
+
 
 app = Flask(__name__)
 
 @app.route("/home")
 def hello():
+    uname='rohit'
+    upass = hashlib.sha1('hello').hexdigest()
     db=connect()
     cursor = db.cursor()
-    cursor.execute("select * from user")
-    user = [dict(uname=row[1],upass=row[2]) for row in cursor.fetchall()]
-    return render_template('home.html',user=user)
+    cursor.execute("""select * from user where username='%s' and password='%s' """ % (uname,upass))
+    result = cursor.fetchone()
+    
+    return render_template('home.html',tuples=cursor.rowcount)
 if __name__=='__main__':
     app.run()
 
--- login.py
+++ login.py
@@ -1,19 +1,38 @@
 from flask import Flask,render_template,request,session,g,url_for,redirect,flash,abort
 from connector import connect
+import hashlib
 
 app=Flask(__name__)
 
 @app.route("/login",methods=['POST','GET'])
 def login():
-    return render_template('login.html')
+    return render_template("login.html")
 
 @app.route("/home",methods=['POST','GET'])
-def submit():
+def home():
     error = None
     if request.method =='POST':
         uname = request.form['username']
         upass = request.form['password']
-    return render_template("home.html",user=uname,passw=upass)
+    passwd = hashlib.sha1(upass).hexdigest()
+    
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("""select * from user where email='%s' and password='%s'""" % (uname,passwd))
+    result = cursor.fetchone()
+    
+    '''the rowcount attribute works only after the row has been fetched that is after fetchone() method '''
+    number_of_rows = cursor.rowcount
+
+    if number_of_rows != 1:
+        '''flash('Invalid email or password!')'''
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('redirecthome'))
+
+@app.route("/redirecthome",methods=['POST','GET'])
+def redirecthome():
+    return render_template("home.html")
 
 if __name__=='__main__':
     app.run()
