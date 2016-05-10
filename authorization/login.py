ef8c2d70c36a217c07c2ac6973680e36b9c36563
0xFN89 <eighteen089@gmail.com>
2016-05-10 15:42:19 +0530

    setup a modular structure with Blueprint

--- authorization/login.py
+++ authorization/login.py
@@ -1,16 +1,20 @@
-from flask import Flask,render_template,request,session,g,url_for,redirect,flash,abort
+from flask import Flask,Blueprint,render_template,request,session,g,url_for,redirect,flash,abort
 from connector import connect
 import hashlib
+import json
 
 app=Flask(__name__)
-app.config.from_pyfile('config.py')
+'''with open('config.json','r') as f:
+            data = json.load(f)'''
+app.config.from_json('config.json')
 app.secret_key = app.config['SECRET_KEY']
+auth = Blueprint('authorization',__name__)
 
-@app.route("/login",methods=['POST','GET'])
+@auth.route("/login",methods=['POST','GET'])
 def login():
-    return render_template("login.html")
+    return render_template("authorization/login.html")
 
-@app.route("/home",methods=['POST','GET'])
+@auth.route("/home",methods=['POST','GET'])
 def home():
     error = None
     if request.method =='POST':
@@ -28,13 +32,13 @@ def home():
 
     if number_of_rows != 1:
         flash('Invalid email or password!')
-        return redirect(url_for('login'))
+        return redirect(url_for('authorization.login'))
     else:
-        return redirect(url_for('redirecthome'))
+        return redirect(url_for('authorization.redirecthome'))
 
-@app.route("/redirecthome",methods=['POST','GET'])
+@auth.route("/redirecthome",methods=['POST','GET'])
 def redirecthome():
-    return render_template("home.html")
+    return render_template("authorization/home.html")
 
 if __name__=='__main__':
     app.run()
